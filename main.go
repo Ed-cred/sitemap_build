1669071ff2c8567ffe21511902c4c765568b960b
Ed-cred <edwardandrew63@gmail.com>
2023-09-26 15:52:06 +0300

    xml output for the links in the sitemap

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/xml"
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 
 	link "github.com/Ed-cred/html_link_parser"
@@ -18,15 +21,35 @@ import (
 4. Filter out links that lead to different domains
 5. Find all pages contained in the site
 */
+const xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9"
+
+type loc struct {
+	Value string `xml:"loc"`
+}
+
+type urlset struct {
+	Urls  []loc  `xml:"url"`
+	Xmlns string `xml:"xmlns,attr"`
+}
 
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "the URL that you want to build a sitemap for")
 	maxDepth := flag.Int("depth", 9, "maximum number of links to traverse")
 	flag.Parse()
 	pages := bfs(*urlFlag, *maxDepth)
-	for _, page := range pages {
-		fmt.Println(page)
+	toXml := urlset{
+		Xmlns: xmlns,
 	}
+	for _, page := range pages {
+		toXml.Urls = append(toXml.Urls, loc{page})
+	}
+	fmt.Print(xml.Header)
+	enc := xml.NewEncoder(os.Stdout)
+	enc.Indent("", "  ")
+	if err := enc.Encode(toXml); err != nil {
+		log.Println("failed to encode sitemap", err)
+	}
+	fmt.Println()
 }
 
 type empty struct{}
@@ -39,13 +62,18 @@ func bfs(urlStr string, maxDepth int) []string {
 	}
 	for i := 0; i <= maxDepth; i++ {
 		q, nq = nq, make(map[string]empty)
+		if len(q) == 0 {
+			break
+		}
 		for url := range q {
 			if _, ok := seen[url]; ok {
 				continue
 			}
 			seen[url] = empty{}
 			for _, link := range get(url) {
-				nq[link] = empty{}
+				if _, ok := seen[link]; !ok {
+					nq[link] = empty{}
+				}
 			}
 
 		}
